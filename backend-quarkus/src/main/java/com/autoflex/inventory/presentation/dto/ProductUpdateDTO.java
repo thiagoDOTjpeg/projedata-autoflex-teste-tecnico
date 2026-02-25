9184c51082556f183ed15300d5e3b084d597697d
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-02-25 15:43:41 -0300

    fix: improved product update dto to verify if isn't blank

--- backend-quarkus/src/main/java/com/autoflex/inventory/presentation/dto/ProductUpdateDTO.java
+++ backend-quarkus/src/main/java/com/autoflex/inventory/presentation/dto/ProductUpdateDTO.java
@@ -1,11 +1,13 @@
 package com.autoflex.inventory.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public record ProductUpdateDTO(
         @Size(min = 1, message = "If provided, the name cannot be empty")
+        @NotBlank(message = "If provided, the name cannot be blank")
         String name,
 
         @Positive(message = "If provided, the price must be greater than zero")
